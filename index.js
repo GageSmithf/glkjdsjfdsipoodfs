627790a565704c715f027ece22ebf55c3d9c21c4
GageSmithf <165711302+GageSmithf@users.noreply.github.com>
2024-05-28 05:37:51 -0500

    updated - check code is 93336269718729

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 // Replace the URL and cookies with your actual values
-const targetURL = 'https://nowgg.nl/';
+const targetURL = 'https://maths.media/';
 
 const cookies = [
   // ... (your cookie objects here)
